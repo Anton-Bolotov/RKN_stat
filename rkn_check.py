b5e00b96c2726d2e22349a1979c0a66e598a04f8
Evgeniia <tulpansik@mail.ru>
2020-07-11 21:24:31 +0300

    Чекер домена на блокировку в РКН v.0.8.1

--- rkn_check.py
+++ rkn_check.py
@@ -1,144 +1,156 @@
-import os
-import sys
-import requests
+from bs4 import BeautifulSoup
+from threading import Thread
+
+import threading
 import time
-
-list_rkn = ['https://raw.githubusercontent.com/zapret-info/z-i/master/dump-00.csv',
-            'https://raw.githubusercontent.com/zapret-info/z-i/master/dump-01.csv',
-            'https://raw.githubusercontent.com/zapret-info/z-i/master/dump-02.csv',
-            'https://raw.githubusercontent.com/zapret-info/z-i/master/dump-03.csv',
-            'https://raw.githubusercontent.com/zapret-info/z-i/master/dump-04.csv',
-            'https://raw.githubusercontent.com/zapret-info/z-i/master/dump-05.csv',
-            'https://raw.githubusercontent.com/zapret-info/z-i/master/dump-06.csv',
-            'https://raw.githubusercontent.com/zapret-info/z-i/master/dump-07.csv',
-            'https://raw.githubusercontent.com/zapret-info/z-i/master/dump-08.csv',
-            # 'https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv',
-            ]
+import requests
 
 
-def file_write(rkn):
-    print('Копирование базы данных РКН...')
-    with open('dump_base_rkn.txt', 'w', encoding='utf-8') as file:
-        for dump in rkn:
-            r = requests.get(dump)
-            file.write(r.text)
-    print('База данных РКН скопированна...')
+class Download_rkn_base(Thread):
+
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+
+    def run(self):
+        r = requests.get(url=self.url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        result = str(soup.find('pre'))
+        with open(file='rkn_base.txt', mode='a', encoding='utf-8') as file:
+            file.write(result)
 
 
-def create_list_of_search(file_name):
-    some_list = []
-    count = 4
-    check = os.stat(file_name).st_size == 0
-    if check:
-        print('\nФайл пустой!')
-        for _ in range(4):
-            count -= 1
-            sys.stdout.write('\r' + 'Завершение программы через - ' + str(count) + ' секунд(ы)')
-            time.sleep(1)
-            sys.stdout.flush()
-        sys.exit()
-    else:
-        with open(file_name, 'r', encoding='utf-8') as file:
-            ff = [i.strip() for i in file]
-            for urls in ff:
-                if '//' in urls:
-                    a = urls.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0]
-                    some_list.append(a)
+class Rkn_check:
+
+    def __init__(self):
+        self.file_in = 'input.txt'
+        self.temporary_blocking = {}
+        self.permanent_blocking = {}
+        self.result_dict = {}
+        self.domains_set = set()
+
+    def creation_set_domains(self):
+        with open(file=self.file_in, mode='r', encoding='utf-8') as file_for_filtration:
+            for domain in file_for_filtration:
+                domain = domain.replace('\n', '')
+                if '/' in domain:
+                    domain = domain.split('/')[2].replace('www.', '')
+                    self.domains_set.add(domain)
                 else:
-                    b = urls
-                    some_list.append(b)
-    return some_list
+                    domain = domain.replace('www.', '')
+                    self.domains_set.add(domain)
 
+    def _status_check(self, record_rkn, domain_name):
+        record_rkn = record_rkn.split(';')
+        if record_rkn[3] == 'Роспотребнадзор':
+            self.temporary_blocking.update({domain_name: 'Временная'})
+        elif record_rkn[3] == 'Минкомсвязь':
+            self.permanent_blocking.update({domain_name: 'Вечная'})
+        elif record_rkn[3] == 'ФНС':
+            self.temporary_blocking.update({domain_name: 'Временная'})
+        elif record_rkn[3] == 'суд':
+            self.temporary_blocking.update({domain_name: 'Временная'})
+        elif record_rkn[3] == 'Роскомнадзор':
+            self.temporary_blocking.update({domain_name: 'Временная'})
+        elif record_rkn[3] == 'Генпрокуратура':
+            self.permanent_blocking.update({domain_name: 'Вечная'})
+        elif record_rkn[3] == 'Мосгорсуд':
+            if 'решение суда по делу № 3' in record_rkn[4]:
+                self.permanent_blocking.update({domain_name: 'Вечная'})
+            elif 'определение о предварительном обеспечении по заявлению № 2' in record_rkn[4]:
+                self.temporary_blocking.update({domain_name: 'Временная'})
+        elif record_rkn[3] == 'МВД':
+            self.temporary_blocking.update({domain_name: 'Временная'})
+        elif record_rkn[3] == 'ФСКН':
+            self.temporary_blocking.update({domain_name: 'Временная'})
+        elif record_rkn[3] == 'Росалкогольрегулирование':
+            self.temporary_blocking.update({domain_name: 'Временная'})
+        elif record_rkn[3] == 'Росмолодежь':
+            self.temporary_blocking.update({domain_name: 'Временная'})
 
-def check_domain(block, good_dict, bad_dict, domain, line):
-    if block == '1':
-        good_dict.update({domain: 'Вечная'})
-    elif block == '2':
-        bad_dict.update({domain: 'Временная'})
-    elif block == '3':
-        good_dict.update({domain: 'Вечная'})
-    else:
-        blocked = line.split(';')[4].replace('�', '').replace(' ', '').split('-')[1]
-        if blocked == '17':
-            good_dict.update({domain: 'Вечная'})
-        elif line.split(';')[3] == '�' * 14:
-            good_dict.update({domain: 'Вечная'})
-        else:
-            if line.split(';')[3] == '���':
-                bad_dict.update({domain: 'Временная'})
+    def domain_check(self, domain, search_line):
+        try:
+            need_domain = search_line.split(';')[1]
+            new_domain = 'www.' + domain
+            new_domain2 = '*.' + str(domain.split('.')[1:]).replace('[', '').replace(
+                ']', '').replace(', ', '.').replace("'", '')
+            if domain == need_domain:
+                self._status_check(record_rkn=search_line, domain_name=domain)
+            elif new_domain == need_domain:
+                self._status_check(record_rkn=search_line, domain_name=domain)
+            elif new_domain2 == need_domain:
+                self._status_check(record_rkn=search_line, domain_name=domain)
             else:
-                print(f'{domain} c этим доменом что-то не так, не могу обработать')
+                self.result_dict.update({domain: 'Нету в базе'})
+        except IndexError:
+            pass
 
-
-def count_rkn():
-    count = 0
-    with open('dump_base_rkn.txt', 'r', encoding='utf-8') as file:
-        for _ in file:
-            count += 1
-    return count
-
-
-def file_read(file_name, file_out):
-    print('Проверка записей...')
-    good_dict = {}
-    bad_dict = {}
-    final_dict = {}
-    count = count_rkn()
-    count2 = 0
-    with open('dump_base_rkn.txt', 'r', encoding='utf-8') as file:
-        for line in file:
-            line = line.replace('\n', '')
-            count2 += 1
-            sys.stdout.write('\r' + 'Проверенно записей РКН - ' + str(count2) + ' из ' + str(count) +
-                             ' (' + str(round(count2 / count * 100, 4)) + ' %)')
-            sys.stdout.flush()
-            for domain in create_list_of_search(file_name):
+    def result_dict_create(self):
+        if 'Вечная' in self.permanent_blocking.values():
+            for key, value in self.permanent_blocking.items():
+                if key in self.temporary_blocking.keys():
+                    self.result_dict.update({key: value + self.temporary_blocking[key]})
+                else:
+                    self.result_dict.update({key: value})
+            for key2, value2 in self.temporary_blocking.items():
                 try:
-                    if domain.replace('www.', '') in line:
-                        block = line.split(';')[4].replace('�', '').replace(' ', '').split('-')[0]
-                        need_domain = line.split(';')[1].replace('www.', '')
-                        if domain.replace('www.', '') == need_domain:
-                            check_domain(block=block, good_dict=good_dict, bad_dict=bad_dict, domain=domain, line=line)
-                        else:
-                            new_domain = '*.' + str(domain.replace('www.', ''))
-                            if new_domain == need_domain:
-                                check_domain(block=block, good_dict=good_dict, bad_dict=bad_dict, domain=domain, line=line)
-                    else:
-                        final_dict.update({domain: 'Нет в базе РКН / домен на русском'})
-                except:
-                    final_dict.update({domain: 'Не могу обработать'})
-        if 'Вечная' in good_dict.values():
-            for key, value in good_dict.items():
-                if key in bad_dict.keys():
-                    final_dict.update({key: value + bad_dict[key]})
-                else:
-                    final_dict.update({key: value})
-            for key2, value2 in bad_dict.items():
-                if final_dict[key2] != 'ВечнаяВременная':
-                    final_dict.update({key2: value2})
+                    if self.result_dict[key2] != 'ВечнаяВременная':
+                        self.result_dict.update({key2: value2})
+                except KeyError:
+                    self.result_dict.update({key2: value2})
         else:
-            for keys, values in bad_dict.items():
-                final_dict.update({keys: values})
+            for keys, values in self.temporary_blocking.items():
+                self.result_dict.update({keys: values})
 
-        print('\nЗапись данных в файл... output.txt')
-        with open(file_out, 'w', encoding='utf-8') as files:
-            for keys, values in final_dict.items():
-                if 'ВечнаяВременная' == values:
-                    files.write(str(keys) + '\t' + str(values).replace('ВечнаяВременная', 'Вечная') + '\n')
-                else:
-                    files.write(str(keys) + '\t' + str(values) + '\n')
+    def result_dict_sort(self):
+        print(self.result_dict)
+        for key, values in self.result_dict.items():
+            if self.result_dict[key] == 'ВечнаяВременная':
+                self.result_dict.update({key: 'Вечная'})
+
+    def run(self):
+        with open(file='rkn_base.txt', mode='r', encoding='utf-8') as file_for_filtration:
+            for search_line in file_for_filtration:
+                for domain in self.domains_set:
+                    self.domain_check(domain=domain, search_line=search_line)
 
 
-first_time = time.time()
-file_write(list_rkn)
-file_read('input.txt', 'output.txt')
-print('Удаление базы данных РКН с ПК...')
-time.sleep(0.5)
-os.remove('dump_base_rkn.txt')
-last_time = time.time()
-program_time = round(last_time - first_time, 1)
-if program_time >= 60:
-    print('Время затраченное на поиск -', round(program_time // 60), 'минут(ы)', round(program_time % 60), 'секунд(а)')
-else:
-    print('Время затраченное на поиск -', program_time, 'секунд(а)')
-input('Для выхода из программы нажмите Enter')
+base_rkn = [
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-00.csv',
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-01.csv',
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-02.csv',
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-03.csv',
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-04.csv',
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-05.csv',
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-06.csv',
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-07.csv',
+    'https://sourceforge.net/p/z-i/code-0/HEAD/tree/dump-08.csv',
+]
+
+if __name__ == '__main__':
+    start_time = time.time()
+    threads_domains = []
+    for urls in base_rkn:
+        thread = Download_rkn_base(url=urls)
+        thread.start()
+        threads_domains.append(thread)
+        if threading.active_count() >= 350:
+            for t in threads_domains:
+                t.join()
+    for t in threads_domains:
+        t.join()
+
+    end_time = time.time()
+    print(f'Время затраченное на скачивание базы ркн - ', round(end_time - start_time, 2), 'секунд')
+
+    start_time1 = time.time()
+    rkn = Rkn_check()
+    rkn.creation_set_domains()
+    rkn.run()
+    rkn.result_dict_create()
+    rkn.result_dict_sort()
+
+    print(rkn.result_dict)
+
+    end_time1 = time.time()
+    print(f'Время затраченное на выполнение программы - ', round(end_time1 - start_time1, 2), 'секунд')
